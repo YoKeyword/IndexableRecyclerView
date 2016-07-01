bf604940edeae4b3989aa9f2b3c8d9669d351265
youyouwang <328903522@qq.com>
2016-07-01 14:31:00 +0800

    Update demo

--- sample/src/main/java/com/yokeyword/sample/city/CityEntity.java
+++ sample/src/main/java/com/yokeyword/sample/city/CityEntity.java
@@ -6,4 +6,15 @@ import me.yokeyword.indexablelistview.IndexEntity;
  * Created by YoKeyword on 16/3/20.
  */
 public class CityEntity extends IndexEntity {
+    private String name;
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- sample/src/main/java/com/yokeyword/sample/contact/ContactEntity.java
+++ sample/src/main/java/com/yokeyword/sample/contact/ContactEntity.java
@@ -6,11 +6,12 @@ import me.yokeyword.indexablelistview.IndexEntity;
  * Created by YoKeyword on 16/3/24.
  */
 public class ContactEntity extends IndexEntity {
+    private String name;
     private String avatar;
     private String mobile;
 
     public ContactEntity(String name, String mobile) {
-        super(name);
+        this.name = name;
         this.mobile = mobile;
     }
 
@@ -28,5 +29,15 @@ public class ContactEntity extends IndexEntity {
 
     public void setMobile(String mobile) {
         this.mobile = mobile;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
     }
 }
