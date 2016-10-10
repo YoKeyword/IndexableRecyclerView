db050fdc5c342c6dc5bc6ed6bb8824253756dacf
王右右 <328903522@qq.com>
2016-10-10 10:11:07 +0800

    [城市选择Demo]初始化时显示 进度条

--- sample/src/main/java/me/yokeyword/sample/city/PickCityActivity.java
+++ sample/src/main/java/me/yokeyword/sample/city/PickCityActivity.java
@@ -5,6 +5,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.view.View;
+import android.widget.ProgressBar;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -24,6 +25,7 @@ public class PickCityActivity extends AppCompatActivity {
     private List<CityEntity> mDatas;
     private SearchFragment mSearchFragment;
     private SearchView mSearchView;
+    private ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -34,6 +36,7 @@ public class PickCityActivity extends AppCompatActivity {
         mSearchFragment = (SearchFragment) getSupportFragmentManager().findFragmentById(R.id.search_fragment);
         IndexableLayout indexableLayout = (IndexableLayout) findViewById(R.id.indexableLayout);
         mSearchView = (SearchView) findViewById(R.id.searchview);
+        mProgressBar = (ProgressBar) findViewById(R.id.progress);
 
         // setAdapter
         CityAdapter adapter = new CityAdapter(this);
@@ -45,6 +48,7 @@ public class PickCityActivity extends AppCompatActivity {
             public void onFinished(List<CityEntity> datas) {
                 // 数据处理完成后回调
                 mSearchFragment.bindDatas(mDatas);
+                mProgressBar.setVisibility(View.GONE);
             }
         });
 
